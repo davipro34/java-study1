b79878c24002851382cd5e68f7c840f5fba8124c
David Atlan <david@davipro.fr>
2023-12-07 18:32:19 +0100

    Say Bonjour without loop

---
+++ loop_for/OC.java
@@ -0,0 +1,11 @@
+package loop_for;
+
+public class OC {
+    public static void main(String[] args) {
+        bonjour("Jean");
+    }
+    
+    static void bonjour(String nom) {
+        System.out.println("Bonjour " + nom + " !!");
+    }
+}
