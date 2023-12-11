ddf2ba406bc28a842aaf65f23644f24a55f34afa
David Atlan <david@davipro.fr>
2023-12-11 08:24:03 +0100

    first for loop

--- loop_for/OC.java
+++ loop_for/OC.java
@@ -3,6 +3,9 @@ package loop_for;
 public class OC {
     public static void main(String[] args) {
         bonjour("Jean");
+        for(int i = 0; i <= 999; i++) {
+            bonjour(Integer.toString(i));
+        }
     }
     
     static void bonjour(String nom) {
