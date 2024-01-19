63d06f79728feab4fb2e9a16f0e574f8cfd9822a
David Atlan <david@davipro.fr>
2024-01-19 11:14:36 +0100

    Add displaySum and calculatePerimeter methods

--- sandbox/Play.java
+++ sandbox/Play.java
@@ -9,10 +9,31 @@ public class Play {
         System.out.println("Hello World");
         displayPerimeter(10, 11);
 
+        int sumOfSmallNumbers = displaySum(3,7);
+        System.out.println(sumOfSmallNumbers); // -> 10
+
+        int sumOfLargerNumbers = displaySum(sumOfSmallNumbers,999);
+        System.out.println(sumOfLargerNumbers); // -> 1009
+
+        int myPerimeter = calculatePerimeter(10, 12);
+        System.out.println(myPerimeter);
+
+        System.out.println(calculatePerimeter(10, 12));
+
     }
 
     public static void displayPerimeter(int length, int width) {
       int perimeter = 2 * (length + width);
       System.out.println(perimeter);
     }
+
+    public static int displaySum(int length, int width) {
+      return length + width;
+    }
+
+    public static int calculatePerimeter(int length, int width) {
+      int premimeter = 2 * (length + width);
+      return premimeter;
+    }
+
 }
